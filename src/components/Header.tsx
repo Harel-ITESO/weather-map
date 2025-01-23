3852d29e222cca55fa60a630a702feaa701508ec
VMLAP\VMLap <victor.telles@iteso.mx>
2025-01-23 17:54:23 -0600

    Header & Footer created and implements in layout.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+//Header simple
+export default function Header() {
+    return (
+        <header className="bg-blue-500 text-white py-4 text-center">
+            <h1 className="text-2xl font-bold">Header Weather Map</h1>
+        </header>
+    );
+}
+
