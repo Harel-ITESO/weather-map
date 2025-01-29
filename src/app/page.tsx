419622fabd6ed8daf9c2c7363825fb2258afc6c2
victortelles <victor.telles@iteso.mx>
2025-01-29 10:33:47 -0600

    Implementacion de los 2 componentes y el servicio de open weather. en el archivo main del proyecto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 "use client";
+import Maps from "@/components/Maps";
+import Weather from "@/components/Weather";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useState } from "react";
 
 export default function Home() {
-  return <div className="text-red-500 text-3xl m-10 font-bold">Hello</div>;
+  const [location, setLocation] = useState<{ lat: number; lng: number } | null>(
+    null
+  );
+
+  //Ubicacion
+  const handleLocationSelect = (lat: number, lng: number) => {
+    setLocation({ lat, lng });
+  };
+
+  //HTML
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          {/*Componente del mapa*/}
+          <Maps onLocationSelect={handleLocationSelect} />
+        </div>
+
+        {/*Componente del clima*/}
+        <div>
+          {location && (
+            <Weather lat={location.lat} lon={location.lng} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
