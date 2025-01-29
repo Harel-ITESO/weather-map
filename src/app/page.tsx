7c3232d5c92003ccc30ff001326714c56c35aa36
harelog3 <harelolguin@MacBook-Pro-de-Harel.local>
2025-01-28 19:29:48 -0600

    initial commit
    very bugged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
 export default function Home() {
-  return <div className="text-red-500 text-3xl m-10 font-bold">Hello</div>;
+  return (
+    <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY || ""}>
+      <Map
+        defaultZoom={13}
+        defaultCenter={{ lat: 20.675351823800902, lng: -103.35586036963122 }}
+        onClick={(e) => {
+          console.log(e.detail);
+        }}
+      ></Map>
+    </APIProvider>
+  );
 }
